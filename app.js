7e78849410d8f6b1dcdf140e3ee02f03ce2fb26f
Andrew <neebet.2015@yandex.ru>
2023-03-06 17:43:01 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js'; //Imports of FireBase
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
+import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
+//import { initializeApp } from 'firebase/app'; //Imports of FireBase
+//import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth';
+//import { getFirestore, addDoc, collection } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({  //FireBase configuration of application
+  apiKey: "AIzaSyAci7cExKpy7KdcaKtV-NynPs3kNWOUegA",
+  authDomain: "currency-f1988.firebaseapp.com",
+  databaseURL: "https://currency-f1988-default-rtdb.firebaseio.com",
+  projectId: "currency-f1988",
+  storageBucket: "currency-f1988.appspot.com",
+  messagingSenderId: "1009009054053",
+  appId: "1:1009009054053:web:3df8a1eef37bac6a70d18c"
+});
+
+const auth = getAuth(firebaseApp); //Get auth
+const db = getFirestore(); //Get DataBase
+
+const logout_btn = document.getElementById("logout-btn");
+const signupForm = document.getElementById("login-form"); //Create user
+signupForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = signupForm["name"].value;
+  const email = signupForm["email"].value;
+  const password = signupForm["password"].value;
+  console.log(email, password);
+  signInWithEmailAndPassword(auth, email, password).then(()=>{
+    showMessage('Success loging.');
+  }).catch(() => {
+    showMessage('Incorrect loging.');
+  })
+})
+
+const messageElement = document.getElementById('message'); //Message Form
+function showMessage(message){  //Message showing
+  messageElement.textContent = message;
+}
+
+function logout() {
+  signOut(auth).then(() => {
+    showMessage('Sign-out successful.');  // Sign-out successful.
+  }).catch((error) => { 
+    showMessage('An error happened.');  // An error happened.
+  });
+}
+logout_btn.addEventListener('click', logout);
+
+onAuthStateChanged(auth, user => { //Check the login state
+  if(user != null) {
+    console.log('loged');
+  } else {
+    console.log('no user');
+  }
+})
---
+++ calculate.js
@@ -0,0 +1,69 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
+import { getFirestore, doc, setDoc, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
+//import { initializeApp } from "./app.js";
+
+const firebaseApp = initializeApp({  //FireBase csonfiguration of application
+    apiKey: "AIzaSyAci7cExKpy7KdcaKtV-NynPs3kNWOUegA",
+    authDomain: "currency-f1988.firebaseapp.com",
+    databaseURL: "https://currency-f1988-default-rtdb.firebaseio.com",
+    projectId: "currency-f1988",
+    storageBucket: "currency-f1988.appspot.com",
+    messagingSenderId: "1009009054053",
+    appId: "1:1009009054053:web:3df8a1eef37bac6a70d18c"
+  });
+  const auth = getAuth(firebaseApp);
+  const db = getFirestore();
+
+
+const messageElement = document.getElementById('message'); //Message Form
+function showMessage(message){  //Message showing
+  messageElement.textContent = message;
+}
+const logElement = document.getElementById('log'); //Message Form
+function showLog(){  //Message showing
+  logElement.textContent = "<a href='login.html'>Login</a>";
+}
+
+const calcForm = document.getElementById("calc-form"); //Calculation Form
+ fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20200302&json') // Get the currency API Endpoint
+ .then(response => response.json()) // Getting json from response
+ .then(data => {  // Showing response to dropdown with forEach
+   const currencyDropdown = calcForm["currency"];
+   data.forEach(currency => {
+     const option = document.createElement('option');
+     option.value = currency.rate.toFixed(1);
+     option.innerHTML = currency.cc;
+     currencyDropdown.appendChild(option);
+   });
+ })
+ .catch(error => {
+   console.error('Error:', error);
+ });
+
+ calcForm.addEventListener('submit', e => { // Event listener "on click" and calculating currency to amount
+  e.preventDefault();
+  const rate = calcForm["currency"].value;
+  const amount = calcForm["amount"].value;
+  const result = calcForm["result"];
+  result.value = amount * rate 
+  onAuthStateChanged(auth, user => {  // If user loged result will be sended to current history list results of calculation
+    if (user) { 
+      if(result.value != "NaN" ) { // Also checking for Not A Number 
+        addDoc(collection(db, 'users/', user.uid ,'/history'), {
+          Amount: amount,
+          Result: result.value,
+        }).then(()=>{
+          console.log('success');
+        }).catch(err=>{
+           console.log(err.message);
+         })
+      } else {
+        console.log('Dont use Not a Number chars again, understand me, fucking punk?')
+      } } else {
+            console.log('you dont even a user, fuck you, why you pretending like being you its ok?');
+            showMessage('You are not loged');
+            showLog();
+        }
+  })
+})
