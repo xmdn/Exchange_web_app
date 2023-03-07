e646efec2a56776900436eda0139ec84708b6398
Andrew <neebet.2015@yandex.ru>
2023-03-07 19:50:48 +0200

    Pages with routes

--- calculate.js
+++ calculate.js
@@ -65,39 +65,5 @@ const calcForm = document.getElementById("calc-form"); //Calculation Form
             showMessage('You are not loged');
             showLog();
         }
-      link.href = '/calculate';
-      link.innerText = 'Calc';
   })
-
 })
-function template (name, templateFunction) {
-  return templates[name] = templateFunction;
-};
-function route (path, template) {
-  if (typeof template === 'function') {
-      return routes[path] = template;
-  }
-  else if (typeof template === 'string') {
-      return routes[path] = templates[template];
-  } else {
-      return;
-  };
-};
-template('calc', function(){
-  calc();
-});
-route('/calculate', 'calc');
-function resolveRoute(route) {
-  try {
-      return routes[route];
-  } catch (e) {
-      throw new Error(`Route ${route} not found`);
-  };
-};
-function router(evt) {
-  let url = window.location.hash.slice(1) || '/';
-  let route = resolveRoute(url);
-
-  route();
-};
-window.addEventListener('load', router);
--- router.js
+++ router.js
@@ -7,10 +7,18 @@ const route = (event) => {
 
 
 const routes = {
-    "/": "/pages/index.html"
+    404: "/pages/404.html",
+    "/": "/pages/index.html",
+    "/calculate": "/pages/calculate.html"
 };
 const handleLocation = async () => {
     const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
 };
 
+window.onpopstate = handleLocation;
 window.route = route;
+
+handleLocation();
