b3fe2420b1b1db94e85974a9128ae16ffc857c76
Andrew <neebet.2015@yandex.ru>
2023-03-08 18:05:45 +0200

    Dynamic routing for scripts and styles

--- router.js
+++ router.js
@@ -4,11 +4,20 @@ const route = (event) => {
     window.history.pushState({}, "", event.target.href);
     handleLocation()
 };
-
-
+const styleMap = {
+    "/calculate": "calculate.css",
+    "/add_user": "add_user.css",
+    "/": "login.css",
+}
+const scriptMap = {
+    "/calculate": "calculate.js",
+    "/add_user": "add_user.js",
+    "/": "login.js",
+  };
+  
 const routes = {
     404: "/pages/404.html",
-    "/": "/pages/index.html",
+    "/": "/pages/login.html",
     "/calculate": "/pages/calculate.html",
     "/add_user": "/pages/add_user.html"
 };
@@ -17,6 +26,22 @@ const handleLocation = async () => {
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-page").innerHTML = html;
+
+    const scriptFile = scriptMap[path]; 
+    if (scriptFile) {
+        const script = document.createElement("script");
+        script.src = `./scripts/${scriptFile}`;
+        script.type = "module";
+        document.body.appendChild(script);
+    }
+    const styleFile = styleMap[path];
+    if (styleFile) {
+        const style = document.createElement("link");
+        style.href = `styles/${styleFile}`;
+        style.rel = "stylesheet";
+        style.type = "text/css";
+        document.body.appendChild(style);
+    }
 };
 
 window.onpopstate = handleLocation;
