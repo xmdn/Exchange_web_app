19acb09cd424467b6343d138f4e8f37bd43aa99e
Andrew <neebet.2015@yandex.ru>
2023-03-13 08:59:42 +0200

    im crying

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,4 +1,5 @@
 //import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js';
+//import { callback } from 'chart.js/dist/helpers/helpers.core';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
 import { getFirestore, doc, setDoc, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
@@ -37,20 +38,24 @@ const calcForm = document.getElementById("calc-form"); //Calculation Form
 //  .then(data => {
 //     console.log(newDate);
 //  })
- fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20200302&json') // Get the currency API Endpoint
- .then(response => response.json()) // Getting json from response
- .then(data => {  // Showing response to dropdown with forEach
-   const currencyDropdown = calcForm["currency"];
-   data.forEach(currency => {
-     const option = document.createElement('option');
-     option.value = currency.rate.toFixed(1);
-     option.innerHTML = currency.cc;
-     currencyDropdown.appendChild(option);
-   });
- })
- .catch(error => {
-   console.error('Error:', error);
- });
+
+ //fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20200302&json') // Get the currency API Endpoint
+
+  fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json') // Get the currency API Endpoint
+  .then(response => console.log(response)) // Getting json from responses
+//  .then(data => {  // Showing response to dropdown with forEach
+//    const currencyDropdown = calcForm["currency"];
+//    data.forEach(currency => {
+//      const option = document.createElement('option');
+//      option.value = currency.rate.toFixed(1);
+//      option.innerHTML = currency.cc;
+//      currencyDropdown.appendChild(option);
+//    });
+//  })
+//  .catch(error => {
+//    console.error('Error:', error);
+//  });
+
 
  calcForm.addEventListener('submit', e => { // Event listener "on click" and calculating currency to amount
   e.preventDefault();
@@ -79,8 +84,21 @@ const calcForm = document.getElementById("calc-form"); //Calculation Form
   })
 });
 function getDataAndDrawChart() {
+  // var xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.onreadystatechange = function() { 
+  //       if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+  //           callback(xmlHttp.responseText);
+  //   }
+
+  //   xmlHttp.open("GET", 'https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=usd&sort=exchangedate&order=desc'); // true for asynchronous 
+  //   xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  //   xmlHttp.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:5500/calculate');
+  //   xmlHttp.send(true);
+  //   console.log(xmlHttp.responseText)
+
+  //xmlHttp.open("GET", 'https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=aud&sort=exchangedate&order=desc&json', false)
   fetch('https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=aud&sort=exchangedate&order=desc&json', {mode: 'no-cors'})
-    .then(console.log(response))
+    .then(response => console.log(response))
     // .then(data => {
     //   const chartData = [];
     //   data.forEach(currency => {
@@ -111,9 +129,9 @@ function getDataAndDrawChart() {
     //     }
     //   });
     // })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .catch(error => {
+        console.error('Error:', error);
+      });
 };
 
 // Example usage
