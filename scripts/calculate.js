63fd1a0a0edb010fb3a908735c8df3a12f8ff756
Andrew <neebet.2015@yandex.ru>
2023-03-15 20:10:23 +0200

    Fixing bugs fetch with promises

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -54,17 +54,18 @@ const chartBtn = document.getElementById("btn-start");
 
 fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json") // Get the currency API Endpoint
   .then((response) => response.json()) // Getting json from responses
-  .then(data => {  // Showing response to dropdown with forEach
+  .then((data) => {
+    // Showing response to dropdown with forEach
     const currencyDropdown = calcForm["currency"];
-    data.forEach(currency => {
-      const option = document.createElement('option');
+    data.forEach((currency) => {
+      const option = document.createElement("option");
       option.value = currency.rate.toFixed(1);
       option.innerHTML = currency.cc;
       currencyDropdown.appendChild(option);
     });
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
 
 calcForm.addEventListener("submit", (e) => {
@@ -104,187 +105,58 @@ calcForm.addEventListener("submit", (e) => {
   });
 });
 // function getDataAndDrawChart() {
- $("#startDate").datepicker();
- $("#endDate").datepicker();
+$("#startDate").datepicker();
+$("#endDate").datepicker();
 
-// $("#startDate").change(function (e) {
-//   console.log(this.value);
-//   let startDate = this.value;
-// });
-
-// $("#endDate").datepicker();
-
-// $("#endDate").change(function (e) {
-//   console.log(this.value);
-//   let endDate = this.value;
-// });
-chartBtn.addEventListener("click", (e) => {
+chartBtn.addEventListener("click", async (e) => {
   let startDate = new Date(Date.parse($("#startDate").val()));
   let endDate = new Date(Date.parse($("#endDate").val()));
   startDate.setMonth(startDate.getMonth() + 1);
+  startDate.setDate(startDate.getDate() + 1);
   endDate.setMonth(endDate.getMonth() + 1);
-  // console.log(startDate.getDay());
-  // console.log(startDate.getMonth());
-  // console.log(startDate.getFullYear());
 
-  //let dateS = (startDate.getFullYear(), startDate.getMonth().toString().padStart(2, '0'), startDate.getDate().toString().padStart(2, '0'));
-  //console.log('Date', dateS);
-  const chartData = [];
-  let chartArr = [];
+  let chartData = [];
   for (
     let currentDate = new Date(startDate);
     currentDate <= endDate;
     currentDate.setDate(currentDate.getDate() + 1)
   ) {
-    startDate.setDate(startDate.getDate() + 1);
-    let dateS = `${startDate.getFullYear()}${startDate
+    let dateS = `${currentDate.getFullYear()}${currentDate
       .getMonth()
       .toString()
-      .padStart(2, "0")}${startDate
-        .getDate()
-        .toString()
-        .padStart(2, "0")}`;
-    console.log("Curr date", currentDate);
-    fetch(
+      .padStart(2, "0")}${currentDate.getDate().toString().padStart(2, "0")}`;
+    let response = await fetch(
       `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${dateS}&json`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-
-        //const curr = data.find(({ cc }) => cc === "USD");
-        data.forEach((element, index) =>
-          //  {console.log(element.exchangedate); console.log(element.rate)}
-          { 
-             if(index===1) {
-              //console.log(element.cc, element.rate)
-            //console.log(element[Object.keys(element)[2]])
-            //chartArr.push(element.exchangedate, element.rate)
-            chartData.push({
-              date: element.exchangedate,
-              rate: element.rate,
-              });
-
-            // chartData.push({
-            //   date: element.exchangedate,
-            //   rate: element.rate,
-            //   })
-           }
-           
-            //sconsole.log('elem date',element.exchangedate);
-            //console.log('elem rate', element.rate);
-            //console.log('element:',element)
-            // if (typeof element === 'object' && element !== null && Array.isArray(element)) {
-            //   element.forEach(item => {
-            //     console.log(item);
-            //   });
-            // } else {
-            //   console.log('element is not an array');
-            // }
-             // => {
-            //   console.log('elem obj key one:', element[Object.keys(element)[0]]);
-            // });
-            //element[Object.keys(element)[0]];
-            //console.log(element);
-            //console.log('elem obj key one:', element[Object.keys(element)[0]]);
-
-            // chartData.push({
-            //   date: chartDate,
-            //   rate: chartRates,
-            // });
-
-            //  chartData.push({
-            //    date: chartArr[0],
-            //    rate: chartArr[1],
-            //    })
-            //   console.log(chartData)
-          }
-        ); 
-        // console.log(chartArr);        
-        //  chartData.push({
-        //   date: chartArr[0],
-        //   rate: chartArr[1],
-        //   });
-          console.log(chartData);
-
-      })
-      .catch((error) => console.error(error));
-      new Chart(document.getElementById("myChart"), {
-        type: "line",
-        data: {
-          labels: chartData.map((chartData) => chartData.exchangedate),
-          datasets: [
-            {
-              label: "Exchange Rates",
-              data: chartData.map((chartData) => chartData.rate),
-              fill: false,
-              borderColor: "rgb(75, 192, 192)",
-              tension: 0.1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
+    );
+    let result = await response.json();
+    let curr = result.find(({ cc }) => cc === "USD");
+    chartData.push({
+      date: curr.date,
+      rate: curr.rate,
+    });
   }
+  
+  console.log(chartData);
+  new Chart(document.getElementById("myChart"), {
+    type: "line",
+    data: {
+      labels: chartData.map((date) => date),
+      datasets: [
+        {
+          label: "Exchange Rates",
+          data: chartData.map((rate) => rate),
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 });
-
-
-
-//$("#btn-start").click(function () {
-
-// var xmlHttp = new XMLHttpRequest();
-//   xmlHttp.onreadystatechange = function() {
-//       if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//           callback(xmlHttp.responseText);
-//   }
-
-//   xmlHttp.open("GET", 'https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=usd&sort=exchangedate&order=desc'); // true for asynchronous
-//   xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-//   xmlHttp.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:5500/calculate');
-//   xmlHttp.send(true);
-//   console.log(xmlHttp.responseText)
-
-//xmlHttp.open("GET", 'https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=aud&sort=exchangedate&order=desc&json', false)
-// fetch('https://bank.gov.ua/NBU_Exchange/exchange_site?start=20220115&end=20220131&valcode=aud&sort=exchangedate&order=desc&json', {mode: 'no-cors'})
-//   .then(response => console.log(response))
-// .then(data => {
-//   const chartData = [];
-//   data.forEach(currency => {
-//     console.log(data);
-//     chartData.push({
-//       date: currency.date,
-//       rate: currency.rate.toFixed(1),
-//     });
-//   });
-//   new Chart(document.getElementById('myChart'), {
-//     type: 'line',
-//     data: {
-//       labels: chartData.map(data => data.date),
-//       datasets: [{
-//         label: 'Exchange Rates',
-//         data: chartData.map(data => data.rate),
-//         fill: false,
-//         borderColor: 'rgb(75, 192, 192)',
-//         tension: 0.1,
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true,
-//         }
-//       }
-//     }
-//   });
-// })
-// .catch(error => {
-//   console.error('Error:', error);
-// });
-
-// Example usage
-//getDataAndDrawChart();
