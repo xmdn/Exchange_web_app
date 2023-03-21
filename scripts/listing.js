6be446cf56ed663a432bafe48b7898938bc4d5eb
Andrew <neebet.2015@yandex.ru>
2023-03-21 15:46:54 +0200

    working with collection dictionary, set\get

--- scripts/listing.js
+++ scripts/listing.js
@@ -47,69 +47,55 @@ const listingContainer = document.getElementById("listing-container"); //
 
 
 
-fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json") // Get the currency API Endpoint
-  .then((response) => response.json()) // Getting json from responses
-  .then((data) => {
-    // Showing response to dropdown with forEach
+// fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json") // Get the currency API Endpoint
+//   .then((response) => response.json()) // Getting json from responses
+//   .then((data) => {
+//     // Showing response to dropdown with forEach
     
-    data.forEach((currency) => {
-      const listedContainer = document.createElement("a")
-  //     href="/calculate"
-  // onclick="route()"
-      const someCurrency = document.createElement("div");
-      const myChart = document.createElement("canvas");
-      listedContainer.id = `block-${currency.cc}`
-        listedContainer.textContent = currency.txt;
-        listedContainer.className = 'block-currency';
-          listingContainer.appendChild(listedContainer);
-        someCurrency.textContent = currency.rate.toFixed(1);
-          someCurrency.className = 'block-price';
-          listedContainer.appendChild(someCurrency);
-          myChart.id = "myChart";
-          myChart.value = `${currency.cc}`;
-          listedContainer.appendChild(myChart);
-    });
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+//     data.forEach((currency) => {
+      
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
 
-calcForm.addEventListener("submit", (e) => {
-  // Event listener "on click" and calculating currency to amount
-  e.preventDefault();
-  const rate = calcForm["currency"].value;
-  const amount = calcForm["amount"].value;
-  const result = calcForm["result"];
-  result.value = amount * rate;
-  onAuthStateChanged(auth, (user) => {
-    // If user loged result will be sended to current history list results of calculation
-    if (user) {
-      if (result.value != "NaN") {
-        // Also checking for Not A Number
-        addDoc(collection(db, "users/", user.uid, "/history"), {
-          Amount: amount,
-          Result: result.value,
-        })
-          .then(() => {
-            console.log("success");
-          })
-          .catch((err) => {
-            console.log(err.message);
-          });
-      } else {
-        console.log(
-          "Dont use Not a Number chars again, understand me, fucking punk?"
-        );
-      }
-    } else {
-      console.log(
-        "you dont even a user, fuck you, why you pretending like being you its ok?"
-      );
-      showMessage("You are not loged");
-      showLog();
-    }
-  });
-});
+// calcForm.addEventListener("submit", (e) => {
+//   // Event listener "on click" and calculating currency to amount
+//   e.preventDefault();
+//   const rate = calcForm["currency"].value;
+//   const amount = calcForm["amount"].value;
+//   const result = calcForm["result"];
+//   result.value = amount * rate;
+//   onAuthStateChanged(auth, (user) => {
+//     // If user loged result will be sended to current history list results of calculation
+//     if (user) {
+//       if (result.value != "NaN") {
+//         // Also checking for Not A Number
+//         addDoc(collection(db, "users/", user.uid, "/history"), {
+//           Amount: amount,
+//           Result: result.value,
+//         })
+//           .then(() => {
+//             console.log("success");
+//           })
+//           .catch((err) => {
+//             console.log(err.message);
+//           });
+//       } else {
+//         console.log(
+//           "Dont use Not a Number chars again, understand me, fucking punk?"
+//         );
+//       }
+//     } else {
+//       console.log(
+//         "you dont even a user, fuck you, why you pretending like being you its ok?"
+//       );
+//       showMessage("You are not loged");
+//       showLog();
+//     }
+//   });
+// });
 // function getDataAndDrawChart() {
 $("#startDate").datepicker();
 $("#endDate").datepicker();
@@ -117,22 +103,21 @@ $("#endDate").datepicker();
 
 let myChart = null;
 
-
+const chartBtn = document.getElementById("btn-start");
+chartBtn.addEventListener("click", async (e) => {
   let startDate = new Date(Date.parse($("#startDate").val()));
   let endDate = new Date(Date.parse($("#endDate").val()));
   startDate.setMonth(startDate.getMonth() + 1);
   startDate.setDate(startDate.getDate() + 1);
   endDate.setMonth(endDate.getMonth() + 1);
 
-  if(myChart) {
-    myChart.destroy();
-  }
+ 
 
-  const selectedOption = currencyDropdown.options[currencyDropdown.selectedIndex];
+  const selectedOption = "USD";
   let chartData = [];
   let dates = [];
   let rates = [];
-  let names = chartData[0].name;
+  let valueMap = new Map();
   if(startDate & endDate) {
     for (
       let currentDate = new Date(startDate);
@@ -149,67 +134,134 @@ let myChart = null;
       );
       let result = await response.json();
       //const selectedValue = currencyDropdown.value;
-      let valutes = new Map();
+
       result.forEach(element =>{
-        valutes.set( element.cc, element.rates)
+        if(!valueMap.has(element.cc)){
+        valueMap.set( element.cc, []);
+        }
+
+        valueMap.get(element.cc).push(rates = element.rate, dates = element.exchangedate);
       })
-      for (let [key, value] of valutes) {
-        if(myChart.value)
-        console.log(key + " goes " + value);
-        curr = result.reduce(() => {
-          chartData.push({
-            date: curr.exchangedate,
-            rate: curr.rate,
-            name: curr.txt,
-          });
-        }, {});
-      }
 
-
+      
     }
-
-
-
     const minRate = Math.min(rates) / 2;
     const maxRate = Math.max(rates) * 2;
     
-    myChart = new Chart(document.getElementById("myChart"), {
-      type: "line",
-      data: {
-        datasets: [
-          {
-            fill: false,
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.2,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: false,
-            min: minRate,
-            max: maxRate,
-          },
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50,
-          },
-        }
-      },
-    });
+      for (let [key, value] of valueMap) {
+        console.log(key + " goes " + value);
+        // rates = valueMap.rate;
+        // dates = valueMap.exchangedate
+        // for(value) {
+        //   chartData.push({
+        //     date: value[1],
+        //     rate: value[0],
+        //     // name: valueMap.txt,
+        //   });
+        // };
+  
+        value.forEach((element) => {
+          console.log(element);
+
+        })
+
+        const listedContainer = document.createElement("a")
+        //     href="/calculate"
+        // onclick="route()"
+            const someCurrency = document.createElement("div");
+            //const myChart = document.createElement("canvas");
+            // listedContainer.id = `block-${currency.cc}`
+              // listedContainer.textContent = currency.txt;
+              listedContainer.className = 'block-currency';
+                listingContainer.appendChild(listedContainer);
+              // someCurrency.textContent = currency.rate.toFixed(1);
+                someCurrency.className = 'block-price';
+                listedContainer.appendChild(someCurrency);
+                //myChart.id = "myChart";
+                // myChart.value = `${currency.cc}`;
+                //listedContainer.appendChild(myChart);
+                // if(myChart) {
+                //   myChart.destroy();
+                // }
+                myChart = new Chart(document.getElementById("myChart"), {
+                  type: "line",
+                  data: {
+                    datasets: [
+                      {
+                        fill: false,
+                        borderColor: "rgb(75, 192, 192)",
+                        tension: 0.2,
+                      },
+                    ],
+                  },
+                  options: {
+                    scales: {
+                      y: {
+                        beginAtZero: false,
+                        min: minRate,
+                        max: maxRate,
+                      },
+                    },
+                    layout: {
+                      padding: {
+                        left: 50,
+                        right: 50,
+                        top: 50,
+                        bottom: 50,
+                      },
+                    }
+                  },
+                });
+
+
+          myChart.data.labels = value[0];
+          // myChart.data.datasets[0].label = chartData[0].name;
+          myChart.data.datasets[0].data = value[1];
+          myChart.update();
+      }
+
 
     
-    myChart.data.labels = dates;
-    myChart.data.datasets[0].label = names
-    myChart.data.datasets[0].data = rates;
-    myChart.update();
+
+
+
+    // const minRate = Math.min(rates) / 2;
+    // const maxRate = Math.max(rates) * 2;
+    
+    // myChart = new Chart(document.getElementById("myChart"), {
+    //   type: "line",
+    //   data: {
+    //     datasets: [
+    //       {
+    //         fill: false,
+    //         borderColor: "rgb(75, 192, 192)",
+    //         tension: 0.2,
+    //       },
+    //     ],
+    //   },
+    //   options: {
+    //     scales: {
+    //       y: {
+    //         beginAtZero: false,
+    //         min: minRate,
+    //         max: maxRate,
+    //       },
+    //     },
+    //     layout: {
+    //       padding: {
+    //         left: 50,
+    //         right: 50,
+    //         top: 50,
+    //         bottom: 50,
+    //       },
+    //     }
+    //   },
+    // });
+
+    
+
   }
   else {
     showMessage("You are not set up dates for chart");
   }
-
+})
