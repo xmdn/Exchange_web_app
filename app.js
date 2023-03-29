4959cd00aeac1f3a6de56d91885bf561f352b9d2
Andrew <tnebilyk@gmail.com>
2023-03-29 19:24:40 +0300

    routing for web site redirection on main page

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 // import fileURLToPath from 'url';
 import express from 'express';
 import history from 'connect-history-api-fallback'
-//import path from 'path';
+import path from 'path';
 import __dirname from 'path';
+
+//const path = require('path');
+
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
@@ -25,17 +28,58 @@ const app = express();
 // });
 app.use(express.static('public'));
 
+
+
+app.get('*', function(req, res){
+    //res.redirect('/');
+    res.status(404).redirect('/');
+    //res.send(res.redirect('/'), 404);
+  });
+  
+
 // Use connect-history-api-fallback middleware to redirect all requests to the SPA's index.html file
 app.use(history({
-  verbose: true
+  verbose: true,
+  index: 'index.html',
+  // Call this function on errors
+  onErrors: (err, req, res, next) => {
+    // Check if the error is a 404 error
+    res.status(404).send('what???');
+    if (err.statusCode === 404) {
+      // Redirect to the home page
+      res.redirect('/');
+    } else {
+      // If it's not a 404 error, let the default handler handle it
+      next();
+    }
+  }
 }));
 
 // Middleware to handle 404 errors and redirect them to the SPA's index.html file
-app.use((req, res, next) => {
-  const indexFile = path.resolve(__dirname, 'public', 'index.html');
-  res.status(404).sendFile(indexFile);
-  res.status(500).sendFile(indexFile);
-});
+// app.use((req, res, next) => {
+//     if (res.status === 404) {
+//         console.log(statusCode);
+//         //res.redirect('/');
+//         resolve(__dirname, 'public', '/');
+//       } 
+//       next();
+//   const indexFile = path.resolve(__dirname, 'public', '/');
+//   res.status(404).sendFile(indexFile);
+//   res.status(500).sendFile(indexFile);
+
+// });
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Middleware to handle 404 errors and redirect them to the SPA's index.html file
+// app.use((req, res, next) => {
+//   if (res.statusCode === 404) {
+//     const indexFile = path.resolve(__dirname, 'public', 'index.html');
+//     res.sendFile(indexFile);
+//   } else {
+//     next();
+//   }
+// });
 
 // Start the server
 app.listen(3000, () => {
