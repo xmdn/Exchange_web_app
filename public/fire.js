17b45b73ba067e3e65cf6da9f46180edd468fec0
Andrew <neebet.2015@yandex.ru>
2023-04-06 22:40:30 +0300

    liltle chgs

--- public/fire.js
+++ public/fire.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 // import { initializeCalculate } from './scripts/calculate.js';
 // import { initialize } from './scripts/listing.js';
