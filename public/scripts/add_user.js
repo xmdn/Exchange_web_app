db053c3c024ebae8967eef12b2e4a506c4ba29cf
Andrew <tnebilyk@gmail.com>
2023-04-12 18:27:01 +0300

    avatar and full name for default email&pass user

--- public/scripts/add_user.js
+++ public/scripts/add_user.js
@@ -1,4 +1,4 @@
-import { auth, db } from "../fire.js";
+import { auth, db, storage } from "../fire.js";
 import {
   onAuthStateChanged,
   createUserWithEmailAndPassword,
@@ -6,19 +6,33 @@ import {
 import {
   doc,
   setDoc,
+  addDoc,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
 
 const signupForm = document.getElementById("signup-form"); //Create user
-signupForm.addEventListener("submit", (e) => {
+signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const name = signupForm["name"].value;
+  const fullname = signupForm["fullname"].value;
   const email = signupForm["email"].value;
   const password = signupForm["password"].value;
+  const avatarRef = ref(storage, `avatars/default.png`);
+  const type = "EmailAndPassword";
+  //const response = await fetch(avatarUrl);
+  //const blob = await response.blob();
+  //await uploadBytes(avatarRef, blob)
+  const userAvatar = await getDownloadURL(avatarRef);
+
+  //const default_avatar = "./icons/default.png"; 
   console.log(email, password);
   createUserWithEmailAndPassword(auth, email, password).then((cred) => {
     return setDoc(doc(db, "users", cred.user.uid), {
+      Type: type,
       Name: name,
+      fullName: fullname,
       Email: email,
+      Avatart: userAvatar,
       Password: password,
     })
       .then(() => {
